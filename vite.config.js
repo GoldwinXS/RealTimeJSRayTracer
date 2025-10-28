5c9d00d9dd0f96e1ade6ce7c005ccc88af8d43f4
Goldwin Stewart <goldwin.stewart@gmail.com>
2025-10-28 15:56:55 -0400

    change to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from "vite";
-import path from "path";
 import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  plugins: [glsl({})],
-  resolve: {
-    alias: {
-      "THREE.js-PathTracing-Renderer": path.resolve(
-        __dirname,
-        "node_modules/THREE.js-PathTracing-Renderer"
-      ),
-    },
-  },
-  optimizeDeps: {
-    exclude: ["js-big-decimal"],
-  },
-  server: {
-    host: true, // Listen on all local IPs
-  },
+  plugins: [glsl()],
+  base: "/RealTimeJSRayTracer/",
 });
